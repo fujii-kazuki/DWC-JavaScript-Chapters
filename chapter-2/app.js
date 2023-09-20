6de8fe068b93b4f48019fa3ee3f6ab3ad569d48a
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-20 17:50:45 +0900

    2章終了

---
+++ chapter-2/app.js
@@ -0,0 +1,58 @@
+// let hello = 'Hello World';
+// alert(hello);
+
+// // 整数
+// let int1 = 1;
+// // 負数
+// let int2 = -10;
+// // 小数点
+// let float1 = 3.14;
+// // 文字列
+// let str1 = 'JavaScriptを覚えよう';
+
+// // 足し算
+// alert(4 + 3);
+
+// // 引き算
+// alert(8 - 5);
+
+// // 掛け算
+// alert(2 * 6);
+
+// // 割り算
+// alert(10 / 2);
+
+// // 文字列結合
+// let str1 = 'Hello';
+// let str2 = 'World';
+// alert(str1 + str2);
+
+// let orange = 120;
+// let apple = 100;
+
+// if (orange < apple) {
+//   alert('みかんの値段がりんごより安い');
+// } else if (orange == apple) {
+//   alert('みかんとりんごが同じ値段');
+// } else {
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max) {
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2をかけ続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++) {
+//   num = num + i;
+// }
+// alert('1から10まで足し算した結果は' + num + 'です');
